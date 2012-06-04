35f0448f8141a460b139462380f65074c5a32178
Sylvain Leroux <sylvain@chicoree.fr>
2012-06-04 18:53:16 +0200

    Changed 'struct-read' to search for min/max temp

--- struct-read.py
+++ struct-read.py
@@ -7,17 +7,38 @@
 #
 
 import struct
+import datetime
+
+import pytz
 
 f = open("history.dat", "rb")
 
 try:
+    origin = datetime.datetime(1899,12,30,0,0,0, tzinfo=pytz.utc)
+    min = None
+    max = None
+
     s = struct.Struct("<dffflfffffffl")
     while True:
 	record = f.read(56)
 	if len(record) != 56:
 	    break;
-    
-	print s.unpack(record)
+
+	fields = s.unpack(record)
+	(timestamp,temp) = (fields[i] for i in (0,8))
+	
+	# adjust date
+	timedelta = datetime.timedelta(timestamp)
+	date = origin + timedelta
+
+	# Record min and max
+	if min is None or min[1] > temp:
+	    min = (date, temp)
+	if max is None or max[1] < temp:
+	    max = (date, temp)
+
+    print "min", min
+    print "max", max
 
 except IOError:
 	# Your error handling here
